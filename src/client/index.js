2f8e9ceac245e3b855ab4d8d79cf4785fe8db74e
arianecrestani <ariane.mrv@gmail.com>
2021-04-06 12:08:20 +0200

    rename css to scss

---
+++ src/client/index.js
@@ -0,0 +1,10 @@
+import { app } from "./js/app";
+import { testing } from "./js/testing";
+
+import "./styles/header.scss";
+import "./styles/main.scss";
+import "./styles/media.scss";
+import "./styles/style.scss";
+
+
+export { app, testing };
