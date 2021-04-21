5d8922138cb3a048e6ca25ebda5b2bd1870d743f
arianecrestani <ariane.mrv@gmail.com>
2021-04-21 14:03:24 +0200

    create div section and grid

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,11 +1,11 @@
 /* Global Variables */
 
 
-  const searchButton = document.getElementById("clickSearch");
-  const startDate = document.getElementById("inputStartDate");
-  const endDate = document.getElementById("inputEndDate");
-  const destination = document.getElementById("destination");
-  
+const searchButton = document.getElementById("clickSearch");
+const startDate = document.getElementById("inputStartDate");
+const endDate = document.getElementById("inputEndDate");
+const destination = document.getElementById("destination");
+
 // Create a new date instance dynamically with JS
 let dt = new Date();
 // creando nova data
@@ -14,8 +14,8 @@ let newDate = dt.getMonth() + 1 + "/" + dt.getDate() + "/" + dt.getFullYear();
 /* Function called by event */
 const generateButtonClick = () => {
   postServerData()
-  // .then((data) => console.log(data))
-  .then((json) => updateUI(json));
+    // .then((data) => console.log(data))
+    .then((json) => updateUI(json));
 };
 
 searchButton.addEventListener("click", generateButtonClick);
@@ -25,7 +25,7 @@ const postServerData = async () => {
   const body = {
     destination: destination.value,
     startDate: startDate.value,
-    endDate: endDate.value
+    endDate: endDate.value,
   };
   return fetch("http://localhost:8000/weathercity", {
     method: "POST",
@@ -33,41 +33,75 @@ const postServerData = async () => {
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(body)
-  })
-  .then((response) => response.json())
-
+    body: JSON.stringify(body),
+  }).then((response) => response.json());
 };
 
 function updateUI(weather) {
   console.log(weather);
 
   const weatherCurrentIcon = document.getElementById("weatherCurrentIcon");
-  const weatherFuturIcon = document.getElementById("weatherFuturIcon");
-  const min_temp  = document.getElementById("min_temp");
-  const max_temp  = document.getElementById("max_temp");
+  const min_temp = document.getElementById("min_temp");
+  const max_temp = document.getElementById("max_temp");
   const currentTemp = document.getElementById("currentTemp");
   const date = document.getElementById("date");
-  const futureTemp = document.getElementById("futureTemp");
   const weatherDescription = document.getElementById("weatherDescription");
   const city = document.getElementById("city");
-  const imagePlace  = document.getElementById("imagePlace")
+  const imagePlace = document.getElementById("imagePlace");
 
-  weatherCurrentIcon.src = `https://www.weatherbit.io/static/img/icons/${weather.weatherCurrentIcon}.png`
-  weatherFuturIcon .innerHTML = `${weather.weatherFuturIcon}`;
-  min_temp .innerHTML = `${weather.min_temp}°C`;
-  max_temp .innerHTML = `${weather.max_temp}°C`;
-  currentTemp.innerHTML = `${weather.currentTemp }°C`;
+  weatherCurrentIcon.src = `https://www.weatherbit.io/static/img/icons/${weather.weatherCurrentIcon}.png`;
+  min_temp.innerHTML = `${weather.min_temp}°C`;
+  max_temp.innerHTML = `${weather.max_temp}°C`;
+  currentTemp.innerHTML = `${weather.currentTemp}°C`;
   date.innerHTML = weather.newDate ? weather.newDate : "";
-  futureTemp.innerHTML = `${weather.futureTemp }°C`;
   weatherDescription.innerHTML = weather.weatherDescription;
-  city.innerHTML = weather.latCity? weather.lngCity : "";
+  city.innerHTML = weather.latCity ? weather.lngCity : "";
   imagePlace.innerHTML = `${weather.imagePlace}`;
- 
 
   destination.value = "";
   startDate.value = "";
-  endDate.value= "";
+  endDate.value = "";
 
-  
+  showFutureTemprature(weather.futureTemp);
+}
+
+const showFutureTemprature = (futureTemp) => {
+  futureTemp.forEach((element) => {
+    showTemperature(element);
+  });
 };
+
+const showTemperature = (dayTemp) => {
+  console.log(dayTemp);
+
+  const container = document.getElementsByClassName("container")[0];
+  container.appendChild(createWeatherView());
+
+
+  // get the div by id
+  // add element to grid
+};
+
+const createWeatherView = (dayTemp) => {
+
+  const section = createDivSection();
+
+
+  // const futureWeatherSection = document.createElement("div");
+  // futureWeatherSection.className = "divSectionTemperature";
+  // futureWeatherSection.id = "WeatherSectionFuture";
+  // futureWeatherSection.appendChild(container);
+  // create the icon
+  // create the description
+  // create the weather
+  // create the max
+  // create the min
+  // return view
+  return section;
+};
+
+function createDivSection() {
+  const section = document.createElement("div");
+  section.className = "divSection";
+  return section;
+}
