595497b32f5ea6e0f24375f6b803231cc326727e
arianecrestani <ariane.mrv@gmail.com>
2021-04-25 21:55:17 +0200

    refactory media css

--- src/client/index.js
+++ src/client/index.js
@@ -2,7 +2,7 @@ import * as App from "./js/app";
 import * as DateHelper from "./js/date";
 
 import "./styles/header.scss";
-import "./styles/media.scss";
 import "./styles/style.scss";
+import "./styles/media.scss";
 
 export { App, DateHelper  };
