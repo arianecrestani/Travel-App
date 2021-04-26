3e5495a527f274f361ac9b63530125feb5d93c9e
arianecrestani <ariane.mrv@gmail.com>
2021-04-26 19:54:50 +0200

    setup test client

--- __test__/date.test.js
+++ __test__/date.test.js
@@ -1,23 +1,24 @@
-import { formatDate } from '../src/client/js/date.js';
-
-describe("Testing the submit functionality", ()=>{
-    test("should be a function() function",()=>{
-        expect(formatDate).toBeDefined();
-    })
-})
-
-
-describe("Testing format Date", ()=>{
-    test("fomatDate should be on this format",()=> {
-        const date = new Date("2000.04.29");
-        expect(formatDate(date)).toBe("29/4/2000");
-       
-    })
-                 
-})
+import {
+  formatDate,
+  calculateDaysBetweenDates,
+} from "../src/client/js/date.js";
 
 
 
+describe("Testing format Date", () => {
+  test("fomatDate should be on this format", () => {
+    const date = new Date("2000.04.29");
+    expect(formatDate(date)).toBe("29/4/2000");
+  });
+});
 
-
-
+describe("Testing how many days ", () => {
+  test("fomatDate should be on this format", () => {
+    expect(
+      calculateDaysBetweenDates(new Date("2000.04.29"), new Date("2000.04.30"))
+    ).toBe(1);
+    expect(
+        calculateDaysBetweenDates(new Date("2000.04.29"), new Date("2000.04.30"))
+      ).toBe(1);
+  });
+});
--- __test__/update.test.js
+++
@@ -1,8 +0,0 @@
-
-// import {updateUI} from '../src/client/js/update.js';
-
-// describe("Testing the submit functionality", ()=>{
-//     test("Testing the handleSubmit() function",()=>{
-//         expect(handleSubmit).toBeDefined();
-//     })
-// })
